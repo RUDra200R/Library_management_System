650c03e2edc46c223e8f75b61f35a495391b8fdf
RUDra200R <127923748+RUDra200R@users.noreply.github.com>
2024-03-06 16:56:08 +0530

    Add files via upload

---
+++ LMS/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
+    <link rel="stylesheet" type="text/css" href="css/header_style.css" />
+</head>
+<body>
+    <header>
+        <a href="./" class="logo-link">
+            <div class="logo">
+                Library Management System
+            </div>
+        </a>
+    </header>
+</body>
+</html>
